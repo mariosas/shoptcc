03257c901baa1eb11e9b5919986a1e27c04809b6
Mário Sérgio <mariosergio952@hotmail.com.br>
2015-12-01 11:02:17 -0300

    Add header at application

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Credentials: true');
+
 \Debugbar::disable();
 
 //Loja
